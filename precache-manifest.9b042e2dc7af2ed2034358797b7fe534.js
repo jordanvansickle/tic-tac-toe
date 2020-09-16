aa3d2a6db88a845ea0b5cb581d32a8bc953a701b
jordanvansickle <jordanvansickle@hotmail.com>
2020-09-15 20:45:08 -0500

    Updates

---
+++ precache-manifest.9b042e2dc7af2ed2034358797b7fe534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c71600a9278f2adb23f37cd27e56cb",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b30fed1971ac8a336ecf",
+    "url": "/tic-tac-toe/static/css/main.5c535923.chunk.css"
+  },
+  {
+    "revision": "d4beeb07b80aeb6b3f4c",
+    "url": "/tic-tac-toe/static/js/2.f47b7d6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.f47b7d6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b30fed1971ac8a336ecf",
+    "url": "/tic-tac-toe/static/js/main.c92b09bf.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
